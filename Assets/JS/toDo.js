0b521ed37b0ceffc9871693f97bbbc2afd9bbbd1
Stephen Finegan <stephenfinegan@Stephens-MacBook-Air.local>
2016-05-20 13:27:20 +0100

    Basic Html, css and jQuery

---
+++ Assets/JS/toDo.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('li').click(function(){
+		$(this).toggleClass('completed');
+
+
+		/*if($(this).css('color') === ('rgb(0, 0, 0)')){
+			$(this).css({
+				color: "rgb(128, 128, 128)",
+				textDecoration: "line-through"
+				})
+			
+		}
+		else{
+			$(this).css({
+				color: "rgb(0, 0, 0)",
+				textDecoration: "none"
+			})
+		}*/
+		
+		
+	})
+
+	
+
+})

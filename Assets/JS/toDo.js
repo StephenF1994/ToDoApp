aaef86055819a1bcaf7ac0162db157bf49eac55a
Stephen Finegan <stephenfinegan@Stephens-MacBook-Air.local>
2016-05-20 14:30:35 +0100

    Add and remove items functioning correctly, No styling yet

--- Assets/JS/toDo.js
+++ Assets/JS/toDo.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-	$('li').click(function(){
+	$('ul').on('click', 'li', function(){							//Elements in 'ul' that may or may not be on the page when it loads. So future 'li' will have same fnctionality
 		$(this).toggleClass('completed');
-
-
-		/*if($(this).css('color') === ('rgb(0, 0, 0)')){
-			$(this).css({
-				color: "rgb(128, 128, 128)",
-				textDecoration: "line-through"
-				})
-			
-		}
-		else{
-			$(this).css({
-				color: "rgb(0, 0, 0)",
-				textDecoration: "none"
-			})
-		}*/
-		
-		
 	})
 
+$('ul').on('click', 'span', function(event){						//Same here, future 'span' will do the same as original 'span'
 	
+	$(this).parent().fadeOut(500, function(){						//on the spans parent(li), fade out .5 seconds then remove.
+		$(this).remove();
+	})
+	event.stopPropagation();													//Stops bubbling up. When span is clicked the li, ul, div parents are not clicked aswell.
+})
+
+$("input[type= 'text']").keypress(function(event){
+	if(event.which === 13){															//IF key pressed is enter
+		var newTask = $(this).val();												//get value of input
+		$(this).val("");															//Input box reset to blank
+		$('ul').append("<li><span>X </span>" + newTask + "</li>");					//Add new li element with value of input
+	}
+})
+
+
+
 
 })
